ecb2e218aaf991ec4732fa65374058331e2803e2
a-sha0234 <asharma_31@hotmail.com>
2022-04-04 17:22:25 +0100

    add routes for viewing all blogs, getting focused blog and for creating a blog

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const blogs = require("./models/blogSchema");
+
 const app = express();
+require("dotenv").config();
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-const url =
-  "mongodb+srv://a:1@cluster0.hldd1.mongodb.net/blogapi?retryWrites=true&w=majority";
+
 mongoose
-  .connect(url, { useNewUrlParser: true })
+  .connect(process.env.DBURL, { useNewUrlParser: true })
   .then((result) => app.listen(port))
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  blogs.find().then((result) => {
+  //get all blogs on index page starting with newest
+  blogs
+    .find()
+    .sort({ createAt: -1 })
+    .then((result) => {
+      res.json(result);
+    });
+});
+
+app.get("/:id", (req, res) => {
+  //get single blog that was clicked on
+  const id = req.params.id;
+  blogs.findById(id).then((result) => {
     res.json(result);
+  });
+});
+
+app.post("/", (req, res) => {
+  //create blog
+  const blog = new blogs({
+    Author: req.body.Author,
+    title: req.body.title,
+    blogtext: req.body.blogtext,
+  }).save((err) => {
+    res.send(err);
   });
 });
